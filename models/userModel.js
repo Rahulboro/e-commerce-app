491300f4c6eff9004f3e7448c4f60d8f86670468
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-09-05 23:34:02 +0530

    model completed

--- models/userModel.js
+++ models/userModel.js
@@ -4,8 +4,31 @@ const userSchema = new mongoose.Schema
 ({
     name:{
         type:String,
-        required: true,
+        required:[true,'Please enter a username'],
         trim:true,
+    },
+    email:{
+        type : String ,  //email is a string data type in mongoDB
+        required: true,
+        unique: true,   //this will ensure that the value of this field must be unqiue across all documents
+    },
+    password:{
+        type:String,    //password is also a string datatype
+        required:true,
+    },
+    phone: {
+        type: Number,//phone number is an integer datatype
+        minlength: [10],//min length for phone numbers should not be less than ten digits
+        required:true,
+    },
+    address:{
+        type:String,
+        required: true,
+    },
+    role:{
+        type:Number,
+        default:0,
     }
-})
+
+}, {timestamps:true} )
 export default mongoose.model('users')
