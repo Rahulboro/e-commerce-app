49a945a9877d05f28a61e4c8c70eaaf2a0668aee
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-09-05 17:48:41 +0530

    connected db

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 import dotenv from 'dotenv'
 import express from 'express';
 import morgan  from 'morgan';
+import connectToDb from './config/db.js';
 
 
 // configure dotenv 
 dotenv.config();
+// config database 
+connectToDb();
 
 // rest Object
 const app = express()
+//middlewares 
+app.use(express.json())
+app.use(morgan(`dev`)); // for parsing application/json
 
 // rest API 
 app.get('/',(req,res)=>{
