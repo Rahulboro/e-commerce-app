18e8b1ed5ab3f15b888373c30779ae1cb1268093
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-09-06 20:45:50 +0530

    authcontroller

--- controller/authController.js
+++ controller/authController.js
@@ -1,1 +1,49 @@
-export const registerControler = () => {};
+import usermodel from '../models/userModel'
+import { hashpassword } from '../utils/authUtils';
+export const registerControler = async(req,res) => {
+    try {
+        const {name,email,password,phone,address}= req.body     
+        //validation
+        if(!name){
+            return res.send({error:"name is required"})
+        } 
+        if(!email){
+            return res.send({error:"email is required"})
+        } 
+        if(!password){
+            return res.send({error:"password is required"})
+        } 
+        if(!phone){
+            return res.send({error:"phone is required"})
+        } 
+        if(!address){
+            return res.send({error:"address is required"})
+        } 
+        //check email exist or not
+        const userexisting = await usermodel.findOne({email})
+        if (userexisting){
+            return res.send(200).send({
+                success:true,
+                message:'User already Registered  '
+            })
+        }
+        //create new user and save in database
+        const hashedPassword = await hashpassword(password)
+        // save 
+        const newuser = new usermodel({name,email,address,password:hashedPassword}).save()
+
+        res.status(201).send({
+            success:true,
+            message:'User Register Successfully',
+            newuser 
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message: "Error in registration",
+            error
+        })
+    }
+};
