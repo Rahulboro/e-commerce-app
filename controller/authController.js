de75d3c7b97ba60ff762ddef7982abc834b65e59
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-09-08 23:49:12 +0530

    500 server error

--- controller/authController.js
+++ controller/authController.js
@@ -1,6 +1,6 @@
 import usermodel from '../models/userModel.js'
-import { hashpassword } from '../utils/authUtils.js';
-import JWT from 'jsonwebtoken';
+import { comparingPassword, hashpassword } from '../utils/authUtils.js';
+import JWT from 'jsonwebtoken'
 export const registerControler = async(req,res) => {
     try {
         const {name,email,password,phone,address}= req.body     
@@ -48,3 +48,47 @@ export const registerControler = async(req,res) => {
         })
     }
 };
+
+// post login 
+
+
+export const loginController = async (req,res) => {
+    try {
+        const {email,password} = req.body
+        if (!email || !password ){
+            return res.status(404).send({
+                success: false,
+                message: 'Invalid Email or Password'
+            })
+        }
+        const existinguser = await usermodel.findOne({email});
+        if(!existinguser){
+            return res.status(404).send({success:false ,message : 'Email Not Found'})
+        }
+        const match = await comparingPassword(password, user.password)
+        if(!match){
+            return res.status(200).send({success:false ,message : 'Wrong password'});
+        }
+        // creating token 
+        const token= JWT.sign({_id:user._id}, process.env.JWTSECRET,{expiresIn:"36h"});
+        res.status(200).send({
+            success:true,
+            message:'Login successfully',
+            user:{
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                address: user.address,
+            },
+            token,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:'Error in Login',
+            error
+        })
+    }
+
+}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import {registerControler} from '../controller/authController.js'
+import {registerControler, loginController} from '../controller/authController.js'
 
  // router object 
 const router = new express.Router()
 
 //routing object
 router.post('/register', registerControler )
+
+// LOGIN POST 
+router.post('/login', loginController)
 
 export default router
