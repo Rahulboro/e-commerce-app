cd7ef4c2c2a3c777012517c32820f1eec23b4304
ujjalbordoloi <ujjalbordoloi90@gmail.com>
2023-09-19 23:58:45 +0530

    middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
-import JWT from 'jsonwebtoken'
-//Routes protecting function 
-export const requireSignIn = async (req,res,next ) => {
-    const decode = JWT.verify(req.bo)
-}
+import JWT from "jsonwebtoken";
+//Routes protecting function
+export const requireSignIn = async (req, res, next) => {
+  const decode = JWT.verify(req.bo);
+};
